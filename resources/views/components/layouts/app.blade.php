da5f206bc475e24eef2d3215d8f47bc352274f06
Rashid Sadq <rashid@gmail.com>
2026-08-12 00:11:24 +0300

    Fix CSS asset loading for localtunnel and reverse proxies

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,7 +10,23 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Noto+Sans+Arabic:wght@400;500;600;700&display=swap" rel="stylesheet">
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if(file_exists(public_path('build/manifest.json')))
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
+        @if(isset($manifest['resources/css/app.css']['file']))
+            <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+        @endif
+        @if(isset($manifest['_fonts-C9MNnjVw.css']['file']))
+            <link rel="stylesheet" href="{{ asset('build/' . $manifest['_fonts-C9MNnjVw.css']['file']) }}">
+        @endif
+        @if(isset($manifest['resources/js/app.js']['file']))
+            <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
+        @endif
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+
     @livewireStyles
 
     <style>
